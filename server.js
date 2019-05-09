ce4972826a1963c91318c007f391f40e38e28c9c
samlimbu <33290648+samlimbu@users.noreply.github.com>
2019-05-09 13:54:19 +0545

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(errorHandler);
 // start server process.env.PORT || 3000;
 //const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
 // app.listen(process.env.PORT || 3000, function(){
