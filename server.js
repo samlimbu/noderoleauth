488dc7afac193210ef192d65bc888fda1f3704a6
samlimbu <33290648+samlimbu@users.noreply.github.com>
2019-05-09 13:55:38 +0545

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ const errorHandler = require('_helpers/error-handler');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
+app.get('/test', function(req,res,next){
+   
+     console.log('*****test***********');
+     console.log(req);
+     console.log('>>>>>>>>>>>>>>>>>>>>');
+     console.log(req.headers);
+     console.log(req.params);
+     console.log(req.query);
+     //res.send('ok test');
+     res.send(req.headers);
+});
 // api routes
 app.use('/users', require('./users/users.controller'));
 
